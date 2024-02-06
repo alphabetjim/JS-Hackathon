69234d52b7a6e235c4c058eefdc8d9b26c44118b
James Bridgwater Court <james.bcourt@outlook.com>
2024-02-06 11:06:35 +0000

    Clean slate for JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-const API_KEY = ''
+// Public & private keys for the currently problematic Marvel API
+const API_KEY = '3bd6c77220aaca905076cd89406735d5';
+const PRIVATE_KEY = '7d1172856769f6dadbf36aba4d3048515dd3ef55';
+
+console.log('JS is connected');
